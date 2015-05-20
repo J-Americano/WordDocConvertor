708383b7c144393cc5acdc09d6dd6ed683e0ee03
Jacob Americano <jacobamer@gmail.com>
2015-05-19 23:17:30 -0400

    Push to Github
    pushing code to github

---
+++ HelpNoteWorker/HelpNoteWorker/ExcelProgram.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace HelpNoteWorker
+{
+    class ExcelProgram
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ensure all Excel documents are saved and closed. Press enter to continue.");
+            Console.ReadLine();
+            string path = "C:\\Users\\americaj\\Documents\\file.xlsx";
+            string s = null;
+            bool success = ProcessFile(path);
+            if (success)
+            {
+                Process[] process = Process.GetProcesses();
+                foreach (Process proc in process)
+                {
+                    s = proc.ProcessName.ToLower();
+                    if (s.CompareTo("excel") == 0)
+                    {
+                        proc.Kill();
+                    }
+                }
+
+                Console.WriteLine("Finished reformatting help documents.");
+            }
+            else
+                Console.WriteLine("No files found.");
+        }
+
+
+        private static bool ProcessFile(string path)
+        {
+            bool success = true;
+            try
+            {
+                object missing = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Application app2 = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook wb = app.Workbooks.Open(path, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing);
+                Workbook xlWorkBook = app2.Workbooks.Add(missing);
+                Worksheet xlWorkSheet = xlWorkBook.Worksheets.get_Item(1);
+                Worksheet sheet = (Worksheet)wb.Worksheets.get_Item(1);
+                int row = 1;
+                int startRow = 1;
+                //int endRow = 10;
+                for (int endRow = 10; endRow < 770; )
+                {
+                    Range excelRange = sheet.get_Range("C" + startRow.ToString(), "C" + endRow.ToString());
+                    
+                    System.Array myVals = (System.Array)excelRange.Cells.Value;
+                    string[] strArray = new string[myVals.Length];
+                    strArray = ConvertToStringArray(myVals);
+                    int i = 1;
+                    foreach (String item in strArray)
+                    {
+                        xlWorkSheet.Cells[row, i] = item;
+                            i++;
+                    }
+                    endRow += 10;
+                    startRow += 10;
+                    row++;
+                }
+
+
+
+                xlWorkBook.SaveAs("C:\\Users\\americaj\\Documents\\JacobAmericano.xlsx");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                success = false;
+            }
+            return success;
+        }
+
+        public string[] GetRange(string range, Worksheet excelWorksheet)
+        {
+            Microsoft.Office.Interop.Excel.Range workingRangeCells =
+              excelWorksheet.get_Range(range, Type.Missing);
+            //workingRangeCells.Select();
+
+            System.Array array = (System.Array)workingRangeCells.Cells.Value2;
+            string[] arrayS = ConvertToStringArray(array);
+
+            return arrayS;
+        }
+
+        static string[] ConvertToStringArray(System.Array values)
+        {
+
+            // create a new string array
+            string[] theArray = new string[values.Length];
+
+            // loop through the 2-D System.Array and populate the 1-D String Array
+            for (int i = 1; i <= values.Length; i++)
+            {
+                if (values.GetValue(i, 1) == null)
+                    theArray[i - 1] = "";
+                else
+                    theArray[i - 1] = (string)values.GetValue(i, 1).ToString();
+            }
+
+            return theArray;
+        }
+
+    }
+}
---
+++ HelpNoteWorker/HelpNoteWorker/Program.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpNoteWorker
+{
+    class Program
+    {
+        public static int i = 0;
+        public static int total = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ensure all Word documents are saved and closed. Press enter to continue.");
+            Console.ReadLine();
+            string path = Directory.GetCurrentDirectory();
+            string s = null;
+            bool success = processDirectory(path);
+            if (success)
+            {
+                Process[] process = Process.GetProcesses();
+                foreach (Process proc in process)
+                {
+                    s = proc.ProcessName.ToLower();
+                    if (s.CompareTo("winword") == 0)
+                    {
+                        proc.Kill();
+                    }
+                }
+
+                Console.WriteLine("Finished reformatting help documents.");
+            }
+            else
+                Console.WriteLine("No files found.");
+        }
+
+        //Create document method
+        private static void ProcessFile(string path)
+        {
+            int output = ++i;
+            Microsoft.Office.Interop.Word.Application app;
+            Microsoft.Office.Interop.Word.Documents doc;
+            Microsoft.Office.Interop.Word.Document document;
+            Microsoft.Office.Interop.Word.Range rng;
+            Microsoft.Office.Interop.Word.Range rng2;
+
+            object saveOption = Microsoft.Office.Interop.Word.WdSaveOptions.wdDoNotSaveChanges;
+            object missing = System.Reflection.Missing.Value;
+            object styleHeading1 = "Heading 1";
+            object oEndOfDoc = "\\startofdoc";
+
+            string title;
+            try
+            {
+                app = new Microsoft.Office.Interop.Word.Application();
+                
+
+                app.Visible = false;
+                app.DisplayAlerts = WdAlertLevel.wdAlertsNone;
+
+                doc = app.Documents;
+                
+                document = doc.Open(path);
+                //adding text to document
+                
+                rng = document.Bookmarks[ref oEndOfDoc].Range;
+                rng2 = document.Bookmarks[ref oEndOfDoc].Range;
+
+                if (rng.Tables.Count >= 1)
+                {
+                    if (rng.Tables[1].Range.Start == rng2.Start)
+                    {
+                        rng.Tables[1].Rows.Add(rng.Tables[1].Rows[1]);
+                        rng.Tables[1].Rows[1].Cells.Merge();
+                    }
+                }
+
+                rng2.set_Style(document.Styles[styleHeading1]);
+
+                title = path.Substring(path.LastIndexOf("\\") + 1);
+
+                title = title.Substring(0, title.LastIndexOf("."));
+
+                if (rng.Tables.Count >= 1)
+                {
+                    if (rng.Tables[1].Range.Start == rng2.Start)
+                    {
+                        rng2.Text = title;
+                        rng2.Tables[1].Rows[1].ConvertToText();
+                    }
+                }
+                else
+                {
+                    rng2.InsertBefore(title);
+                }
+
+                app.NormalTemplate.Saved = true;
+
+                //Save the document
+                document.SaveAs2(path);
+                if (document != null)
+                {
+                    ((_Document)document).Close(ref saveOption, ref missing, ref missing);
+                    while(Marshal.FinalReleaseComObject(document) > 0 );
+                    document = null;
+                }
+
+                if (doc != null)
+                {
+                    while(Marshal.FinalReleaseComObject(doc) > 0);
+                    doc = null;
+                    
+                }
+
+                if (doc != null)
+                {
+                    ((_Application)app).Quit(ref saveOption, ref missing, ref missing);
+                    while(Marshal.FinalReleaseComObject(app) > 0);
+                    app = null;
+                }
+
+                while(Marshal.FinalReleaseComObject(rng) > 0);
+                while (Marshal.FinalReleaseComObject(rng2) > 0) ;
+
+
+                Console.WriteLine(output + "/" + total + " files written");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                
+            }
+        }
+
+
+        private static bool processDirectory(string path)
+        {
+            string[] fileEntries = Directory.GetFiles(path, "*.doc");
+            total = fileEntries.Length;
+            if (!path.Contains("docx") && !path.Contains("?") && !path.Contains("~") && fileEntries.Length > 0)
+            {
+                Console.WriteLine("Starting write process.");
+                //fileEntries.AsParallel().ForAll(ProcessFile);
+                Parallel.ForEach(fileEntries, fileName => ProcessFile(fileName));
+                /*foreach (string fileName in fileEntries)
+                {
+                    ProcessFile(fileName);
+                    i++;
+                    Console.WriteLine(i + "/" + fileEntries.Length + " files written");
+                }*/
+            }
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
